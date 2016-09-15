a731aefd4588b6c920a2c4a47babb2d762c488f6
Philippe Godin <pgodin@iweb.com>
2016-09-15 13:51:23 -0400

    Adds help command support
    This is the initial commit of python-arsenalclient.
    It is highly inspired from https://github.com/openstack/python-ironicclient

---
+++ arsenalclient/tests/unit/test_shell.py
@@ -0,0 +1,60 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+import re
+import sys
+
+import six
+from arsenalclient import exc
+from arsenalclient import shell as arsenal_shell
+from arsenalclient.tests.unit import utils
+from testtools import matchers
+
+BASE_URL = 'http://no.where:5000'
+
+
+class ShellTest(utils.BaseTestCase):
+    re_options = re.DOTALL | re.MULTILINE
+
+    def setUp(self):
+        super(ShellTest, self).setUp()
+
+    def shell(self, argstr):
+        orig = sys.stdout
+        try:
+            sys.stdout = six.StringIO()
+            _shell = arsenal_shell.ArsenalShell()
+            _shell.main(argstr.split())
+        except SystemExit:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            self.assertEqual(0, exc_value.code)
+        finally:
+            out = sys.stdout.getvalue()
+            sys.stdout.close()
+            sys.stdout = orig
+        return out
+
+    def test_help_unknown_command(self):
+        self.assertRaises(exc.CommandError, self.shell, 'help foofoo')
+
+    def test_help(self):
+        required = [
+            '.*?^usage: arsenal',
+            '.*?^See "arsenal help COMMAND" '
+            'for help on a specific command',
+        ]
+        for argstr in ['--help', 'help']:
+            help_text = self.shell(argstr)
+            for r in required:
+                self.assertThat(help_text,
+                                matchers.MatchesRegex(r,
+                                                      self.re_options))
